d760f9d7f2a9746b8b79ba0c7ca72c241416f210
Amir Chebbi <amirchebbi6@gmail.com>
2024-03-05 14:37:42 +0100

    interpage Routing is done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,27 @@
 import { Routes } from '@angular/router';
 import {HomePageComponent} from "./home-page/home-page.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {ContactPageComponent} from "./contact-page/contact-page.component";
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {AuthenticationPageComponent} from "./authentication-page/authentication-page.component";
 
 const routes: Routes = [
   {
     path:  '',
     component: HomePageComponent,
     title: "Mobilotech"
+  },
+  {
+    path: 'authentication',
+    component: AuthenticationPageComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 export default routes;
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,54 @@
 import { Component } from '@angular/core';
-import {RouterModule} from "@angular/router";
+import {RouterLink, RouterModule, RouterOutlet} from "@angular/router";
 import {SlideComponent} from "../slide/slide.component";
 import {CategoryComponent} from "../category/category.component";
+import {elementAt} from "rxjs";
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-    imports: [RouterModule, SlideComponent, CategoryComponent],
+  imports: [RouterModule, RouterLink, RouterOutlet, SlideComponent, CategoryComponent, FooterComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
-})
+  styleUrl: './home-page.component.css',
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translateX(0%), opacity(0)'
+      })),
+      state('out', style({
+        transform: 'translateX(100%), opacity(100%)'
+      })),
+      transition('in => out', animate('300ms ease-in-out')),
+      transition('out => in', animate('300ms ease-in-out'))
+    ])
+  ]})
+
 export class HomePageComponent {
+
+  observer = new IntersectionObserver((entries) => {
+    entries.forEach((entriy) => {
+      if (entriy.isIntersecting) {
+        entriy.target.classList.add('category-l2');
+      } else {
+        entriy.target.classList.remove('category-l2');
+      }
+    });
+  });
+
+  isVisible :boolean = false;
+  categoryObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entriy) => {
+      if (entriy.isIntersecting) {
+        this.isVisible = true;
+      } else {
+        this.isVisible = false;
+      }
+    });
+  });
+  hiddenCategory:any = document.getElementsByClassName('category-l2');
+
+  hiddenElements:any = document.querySelectorAll('.hidden').forEach((element) => this.observer.observe(element))
 
 }
